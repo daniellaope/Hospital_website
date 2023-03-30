ac21d5c81f25e9f9e8808b6cb316c3c1e588d636
daniellaope <davickdenzope4@gmail.com>
2023-03-30 10:01:06 +0300

    'website'

---
+++ main.js
@@ -0,0 +1,26 @@
+var b = document.getElementById("upImage");
+   var uploaded_image = ""
+   var c = document.getElementById("pictures");
+b.addEventListener("change", function() {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        uploaded_image = reader.result;
+        c.style.backgroundImage = `url(${uploaded_image})`;  
+
+    });
+    reader.readAsDataURL(this.files[0]);
+});
+
+
+        window.addEventListener('scroll', function(){
+          const header = document.querySelector('header');
+          header.classList.toggle("sticky", window.scrollY > 0);
+        });
+
+
+        function toggleMenu(){
+         const menuToggle = document.querySelector('.menuToggle');
+         const navigation = document.querySelector('.navigation');
+         menuToggle.classList.toggle('active');
+         navigation.classList.toggle('active');
+        }
